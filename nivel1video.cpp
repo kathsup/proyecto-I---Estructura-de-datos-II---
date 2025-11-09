a09cee8565b41717f51cf576e0840bfd8b607869
kathsup <katherinencarvallo22@gmail.com>
2025-11-09 11:23:29 -0600

    falta musica y juegos pero todo funciona

--- nivel1video.cpp
+++ nivel1video.cpp
@@ -1,4 +1,4 @@
-#include "nivel1video.h"
+/*#include "nivel1video.h"
 #include "ui_nivel1video.h"
 #include <QMediaPlayer>
 #include <QVideoWidget>
@@ -56,17 +56,6 @@ void nivel1Video::inicializarNivel()
 
 }
 
-/*void nivel1Video::keyPressEvent(QKeyEvent *event)
-{
-    if(escenario) {
-        escenario->manejarTecla(event);
-    }
-
-    if (event->key() == Qt::Key_Escape) {
-        emit volverAlMapa();
-    }
-}
-*/
 
 void nivel1Video::keyPressEvent(QKeyEvent *event)
 {
@@ -105,37 +94,6 @@ void nivel1Video::objetosInteractivos() {
 }
 
 
-/*
-void nivel1Video::inicializarVideoPanel()
-{
-    // 🧱 Panel contenedor
-    panelVideo = new QFrame(this);
-    panelVideo->setGeometry(0, 0, 1914, 1080);
-    panelVideo->setStyleSheet(
-        "background-color: rgba(0, 0, 0, 200);"  // fondo oscuro semitransparente
-        "border: 3px solid #fff;"
-        "border-radius: 15px;"
-        );
-    panelVideo->hide();
-    panelVideo->raise();
-
-    // 🎬 Crear el widget de video
-    QVideoWidget *videoWidget = new QVideoWidget(panelVideo);
-    videoWidget->setGeometry(100, 100, 500, 300); // centrado aproximadamente
-
-    // 🎵 Crear el reproductor
-    player = new QMediaPlayer(panelVideo);
-    player->setVideoOutput(videoWidget);
-
-    // Ruta del video
-    player->setSource(QUrl::fromLocalFile("C:/Users/Lenovo/Downloads/videoNivel1.mp4"));
-
-    // 🧭 Opcional: diseño para centrar el video con layout
-    QVBoxLayout *layout = new QVBoxLayout(panelVideo);
-    layout->addWidget(videoWidget, 0, Qt::AlignCenter);
-    panelVideo->setLayout(layout);
-}*/
-
 void nivel1Video::inicializarVideoPanel()
 {
     // 🧱 Panel contenedor
@@ -151,7 +109,7 @@ void nivel1Video::inicializarVideoPanel()
 
     // 🎬 Crear el widget de video
     QVideoWidget *videoWidget = new QVideoWidget(panelVideo);
-    videoWidget->setGeometry(707, 290, 500, 300);
+    videoWidget->setGeometry(300, 120, 500, 500);
 
     // 🔊 Crear salida de audio
     audioOutput = new QAudioOutput(panelVideo);
@@ -165,9 +123,7 @@ void nivel1Video::inicializarVideoPanel()
     player->setSource(QUrl::fromLocalFile("C:/Users/Lenovo/Downloads/videoNivel1.mp4"));
 
     // 🧭 Centrar con layout opcional
-    QVBoxLayout *layout = new QVBoxLayout(panelVideo);
-    layout->addWidget(videoWidget, 0, Qt::AlignCenter);
-    panelVideo->setLayout(layout);
+
 }
 
 
@@ -183,5 +139,93 @@ void nivel1Video::mostrarPanelVideo()
         panelVideo->raise();
         player->play();       // Reproducir el video
     }
+}
+*/
+
+#include "nivel1video.h"
+#include "ui_nivel1video.h"
+
+
+
+nivel1Video::nivel1Video(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::nivel1Video)
+    , escenario(new Escenario(this))
+{
+    ui->setupUi(this);
+    setFocusPolicy(Qt::StrongFocus);
+
+}
+
+nivel1Video::~nivel1Video()
+{
+
+    delete ui;
+}
+
+void nivel1Video::inicializarNivel()
+{
+    // Configurar el escenario dentro del QGraphicsView del UI
+    QPixmap pixFondo("C:/Users/Lenovo/Downloads/fondoVideo.png");
+    escenario->inicializar(ui->graphicsViewVideo, pixFondo);
+
+    ui->graphicsViewVideo->setFocusPolicy(Qt::NoFocus);
+
+    // Crear el personaje
+    QVector<QPixmap> spritesDer = {
+        QPixmap("C:/Users/Lenovo/Downloads/hada1AD.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation),
+        QPixmap("C:/Users/Lenovo/Pictures/hada2AD.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation),
+        QPixmap("C:/Users/Lenovo/Downloads/hada3AD.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation)
+    };
+
+    QVector<QPixmap> spritesIzq = {
+        QPixmap("C:/Users/Lenovo/Pictures/hada1Iz.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation),
+        QPixmap("C:/Users/Lenovo/Pictures/hada2Iz.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation),
+        QPixmap("C:/Users/Lenovo/Pictures/hada3Iz.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation)
+    };
+
+    QVector<QPixmap> spritesArriba = {
+        QPixmap("C:/Users/Lenovo/Downloads/hada4Ar.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation),
+        QPixmap("C:/Users/Lenovo/Downloads/hada5Ar.png").scaled(100,100, Qt::KeepAspectRatio, Qt::SmoothTransformation)
+    };
+
+    objetosInteractivos();
+    escenario->crearPersonaje(spritesDer, spritesIzq, spritesArriba, QPointF(270, 450));
+    this->setFocus();
+}
+
+void nivel1Video::keyPressEvent(QKeyEvent *event)
+{
+    if (escenario) {
+        escenario->manejarTecla(event);
+    }
+
+    if (event->key() == Qt::Key_Escape) {
+        emit volverAlMapa();
+    }
+
+    // Si presiona E, verificar si está cerca del cuadro
+    if (event->key() == Qt::Key_E) {
+        if (personajeCercaDeCuadro()) {
+
+        }
+    }
+}
+
+
+bool nivel1Video::personajeCercaDeCuadro()
+{
+    if (!escenario || !escenario->personaje || !cuadro) return false;
+
+    QPointF posJugador = escenario->personaje->pos();
+    QRectF areaCuadro = cuadro->rect().translated(cuadro->pos());
+
+    return areaCuadro.adjusted(-50, -50, 50, 50).contains(posJugador);
+}
+
+void nivel1Video::objetosInteractivos()
+{
+    cuadro = escenario->scene->addRect(400, 340, 150, 150, Qt::NoPen);
+    cuadro->setBrush(Qt::transparent);
 }
 
--- nivel1video.h
+++ nivel1video.h
@@ -1,4 +1,4 @@
-#ifndef NIVEL1VIDEO_H
+/*#ifndef NIVEL1VIDEO_H
 #define NIVEL1VIDEO_H
 
 #include <QWidget>
@@ -49,6 +49,44 @@ private:
     QMediaPlayer* player = nullptr;  // 🎵 reproductor multimedia
     QAudioOutput* audioOutput = nullptr;
 
+};
+
+#endif // NIVEL1VIDEO_H
+*/
+
+#ifndef NIVEL1VIDEO_H
+#define NIVEL1VIDEO_H
+
+#include <QWidget>
+#include "escenario.h"
+
+
+namespace Ui {
+class nivel1Video;
+}
+
+class nivel1Video : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit nivel1Video(QWidget *parent = nullptr);
+    ~nivel1Video();
+    void inicializarNivel();
+    void objetosInteractivos();
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
+signals:
+    void volverAlMapa();
+
+private:
+    Ui::nivel1Video *ui;
+    Escenario *escenario;
+
+    QGraphicsRectItem* cuadro;
+    bool personajeCercaDeCuadro();
 };
 
 #endif // NIVEL1VIDEO_H
